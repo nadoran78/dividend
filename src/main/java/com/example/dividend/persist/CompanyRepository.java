a825120f97d33076f241f38361a8aaa08a11663e
nadoran78 <nadoran78@gmail.com>
2023-10-24 17:51:42 +0900

    Initial commit

---
+++ src/main/java/com/example/dividend/persist/CompanyRepository.java
@@ -0,0 +1,15 @@
+package com.example.dividend.persist;
+
+import com.example.dividend.persist.entity.CompanyEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
+    boolean existsByTicker(String ticker);
+
+    Optional<CompanyEntity> findByName(String name);
+
+}
