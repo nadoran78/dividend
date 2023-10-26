97f98667dcfae3c9299f3235523945e02feeba0e
nadoran78 <nadoran78@gmail.com>
2023-10-26 23:25:48 +0900

    회원 관리 기능 구현 완료

--- src/main/java/com/example/dividend/service/CompanyService.java
+++ src/main/java/com/example/dividend/service/CompanyService.java
@@ -13,9 +13,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -78,5 +80,16 @@ public class CompanyService {
 
     public void deleteAutocompleteKeyword(String keyword) {
         this.trie.remove(keyword);
+    }
+
+    public String deleteCompany(String ticker) {
+        var company = this.companyRepository.findByTicker(ticker)
+                            .orElseThrow(() -> new RuntimeException("존재하지 않는 회사입니다."));
+        this.dividendRepository.deleteAllByCompanyId(company.getId());
+        this.companyRepository.delete(company);
+
+        this.deleteAutocompleteKeyword(company.getName());
+
+        return company.getName();
     }
 }
--- src/main/java/com/example/dividend/service/FinanceService.java
+++ src/main/java/com/example/dividend/service/FinanceService.java
@@ -3,25 +3,30 @@ package com.example.dividend.service;
 import com.example.dividend.model.Company;
 import com.example.dividend.model.Dividend;
 import com.example.dividend.model.ScrapedResult;
+import com.example.dividend.model.constants.CacheKey;
 import com.example.dividend.persist.CompanyRepository;
 import com.example.dividend.persist.DividendRepository;
 import com.example.dividend.persist.entity.CompanyEntity;
 import com.example.dividend.persist.entity.DividendEntity;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class FinanceService {
 
     private final CompanyRepository companyRepository;
     private final DividendRepository dividendRepository;
+
+    @Cacheable(key = "#companyName", value = CacheKey.KEY_FINANCE)
     public ScrapedResult getDividendByCompanyName(String companyName) {
+        log.info("search company -> " + companyName);
 
         // 1. 회사명을 기준으로 회사 정보를 조회
         CompanyEntity company = this.companyRepository.findByName(companyName)
@@ -31,24 +36,10 @@ public class FinanceService {
         List<DividendEntity> dividendEntities = this.dividendRepository.findAllByCompanyId(company.getId());
 
         // 3. 결과 조합 후 반환
-//        List<Dividend> dividends = new ArrayList<>();
-//        for (var entity: dividendEntities) {
-//            dividends.add(Dividend.builder()
-//                    .date(entity.getDate())
-//                    .dividend(entity.getDividend())
-//                    .build());
-//        }
-
         List<Dividend> dividends = dividendEntities.stream()
-                                                    .map(e -> Dividend.builder()
-                                                            .date(e.getDate())
-                                                            .dividend(e.getDividend())
-                                                            .build())
+                                                    .map(e -> new Dividend(e.getDate(), e.getDividend()))
                                                     .collect(Collectors.toList());
 
-        return new ScrapedResult(Company.builder()
-                                        .ticker(company.getTicker())
-                                        .name(company.getName())
-                                        .build(), dividends);
+        return new ScrapedResult(new Company(company.getTicker(), company.getName()), dividends);
     }
 }
