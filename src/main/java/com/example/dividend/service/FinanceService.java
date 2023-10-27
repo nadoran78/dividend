ea4cb293bdfd7c990d65adf602a4e9bf8e92e761
nadoran78 <nadoran78@gmail.com>
2023-10-27 18:10:52 +0900

    전체 실습 완료

--- src/main/java/com/example/dividend/service/FinanceService.java
+++ src/main/java/com/example/dividend/service/FinanceService.java
@@ -1,5 +1,6 @@
 package com.example.dividend.service;
 
+import com.example.dividend.exception.impl.NoCompanyException;
 import com.example.dividend.model.Company;
 import com.example.dividend.model.Dividend;
 import com.example.dividend.model.ScrapedResult;
@@ -30,7 +31,7 @@ public class FinanceService {
 
         // 1. 회사명을 기준으로 회사 정보를 조회
         CompanyEntity company = this.companyRepository.findByName(companyName)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다"));
+                .orElseThrow(() -> new NoCompanyException());
 
         // 2. 조회된 회사 ID로 배당금 정보 조회
         List<DividendEntity> dividendEntities = this.dividendRepository.findAllByCompanyId(company.getId());
